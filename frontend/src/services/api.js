d413dc715c699cc14555f76546e296a5290476e4
junwooLee <audwls1864@plnc.co.kr>
2025-12-01 16:33:58 +0900

    build Test

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8000/api'
   : '/api';
-
+console.log("API_BASE_URL >> ", API_BASE_URL);
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
